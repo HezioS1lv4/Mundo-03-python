7f287152905c6e8e972ef672a34c1008c7729ee5
Hézio Silva <103318917+HezioS1lv4@users.noreply.github.com>
2022-11-04 19:56:52 -0300

    Add files via upload

---
+++ ex072.py
@@ -0,0 +1,21 @@
+# Guanabara solução
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+        'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete',
+        'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente', end='')
+print(f'Você digitou o número {cont[num]}')
+
+# Minha solução vv
+"""tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+num = int(input('Digite um número entre 0 e 20: '))
+while num < 0 or num > 20:
+    num = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+if num == 1:
+    print('Você digitou o número um')
+elif num == 20:
+    print('Você digitou o número vinte')"""
